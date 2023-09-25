449976bc36a002d2c67bc950c51160c70790e0cd
Nagavasavi Jeepalyam <nj2506@columbia.edu>
2023-09-25 15:01:20 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,9 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    @ratings_to_show = params[:ratings].keys || []
+    @movies = Movie.with_ratings(@ratings_to_show)
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,16 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    uniq.pluck(:rating)
+  end
+
+  def self.with_ratings(ratings_list)
+    # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+    #  movies with those ratings
+    # if ratings_list is nil, retrieve ALL movies
+    if ratings_list.blank?
+      all
+    else
+      where(rating: ratings_list)
+    end
+  end
 end
